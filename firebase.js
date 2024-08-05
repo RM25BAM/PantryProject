1ec0d38e8673673604035648ba404e66b500cb30
Natasha Piedrabuena <natashapiedrabuena@Natashas-MacBook-Pro.local>
2024-08-05 00:19:01 -0400

    added environment variables

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyBzZJWduh-3JL1ocRqjwkyHFegIhLXGyMs",
-    authDomain: "pantryproject-fc5e8.firebaseapp.com",
-    projectId: "pantryproject-fc5e8",
-    storageBucket: "pantryproject-fc5e8.appspot.com",
-    messagingSenderId: "555160869224",
-    appId: "1:555160869224:web:5222b4af3a2580c3945edd",
-    measurementId: "G-GKXHDSY21F"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
   };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
